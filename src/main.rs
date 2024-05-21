d130441558a006ab3e14ec2e13f8ede0c0bc72a5
Simon <simon21.blum@gmail.com>
2024-05-21 12:23:12 +0200

    Starting

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![allow(non_snake_case)]
 
+use chrono::{DateTime, Utc};
 use dioxus::prelude::*;
+use serde::{Deserialize, Serialize};
 use tracing::Level;
 
 #[derive(Clone, Routable, Debug, PartialEq)]
@@ -9,8 +11,52 @@ enum Route {
     Home {},
     #[route("/blog/:id")]
     Blog { id: i32 },
+    #[route("/test")]
+    Test(),
 }
 
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct StoryPageData {
+    #[serde(flatten)]
+    pub item: StoryItem,
+    #[serde(default)]
+    pub comments: Vec<Comment>,
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct Comment {
+    pub id: i64,
+    #[serde(default)]
+    pub by: String,
+    #[serde(default)]
+    pub text: String,
+    #[serde(with = "chrono::serde::ts_seconds")]
+    pub time: DateTime<Utc>,
+    #[serde(default)]
+    pub kids: Vec<i64>,
+    #[serde(default)]
+    pub sub_comments: Vec<Comment>,
+    pub r#type: String,
+}
+
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
+pub struct StoryItem {
+    pub id: i64,
+    pub title: String,
+    pub url: Option<String>,
+    pub text: Option<String>,
+    #[serde(default)]
+    pub by: String,
+    #[serde(default)]
+    pub score: i64,
+    #[serde(default)]
+    pub descendants: i64,
+    #[serde(with = "chrono::serde::ts_seconds")]
+    pub time: DateTime<Utc>,
+    #[serde(default)]
+    pub kids: Vec<i64>,
+    pub r#type: String,
+}
 fn main() {
     // Init logger
     dioxus_logger::init(Level::INFO).expect("failed to init logger");
@@ -37,10 +83,17 @@ fn Home() -> Element {
 
     rsx! {
         Link {
+    class:"hover:underline",
             to: Route::Blog {
                 id: count()
             },
             "Go to blog"
+        }
+        br {}
+        Link {
+    class:"hover:underline",
+            to: Route::Test{},
+            "Go to test"
         }
         div {
             h1 { "High-Five counter: {count}" }
@@ -48,4 +101,65 @@ fn Home() -> Element {
             button { onclick: move |_| count -= 1, "Down low!" }
         }
     }
+}
+
+#[component]
+fn Test() -> Element {
+    rsx! {StoryListing{
+        story: StoryItem  {
+            id: 0,
+            title: "hello hackernews".to_string(),
+        url: None,
+        text: None,
+        by: "Author".to_string(),
+        score: 0,
+        descendants: 0,
+        time: chrono::Utc::now(),
+        kids: vec![],
+        r#type: "".to_string(),
+        }
+    }}
+}
+
+#[component]
+fn StoryListing(story: ReadOnlySignal<StoryItem>) -> Element {
+    let StoryItem {
+        title,
+        url,
+        by,
+        score,
+        time,
+        kids,
+        ..
+    } = &*story.read();
+
+    let url = url.as_deref().unwrap_or_default();
+    let hostname = url
+        .trim_start_matches("https://")
+        .trim_start_matches("http://")
+        .trim_start_matches("www.");
+
+    let score = format!("{score} {}", if *score == 1 { "point" } else { "points" });
+    let time = time.format("%D %l:%M %p");
+
+    let comments = kids.len();
+
+    rsx! {
+            div { padding: "0.5rem", position: "relative",
+                div {
+                font_size: "1.5rem",
+                a { href: url, "{title}"}
+                a {
+            color: "gray",
+            href: "https://news.ycombinator.com/from?site={hostname}",
+            text_decoration: "none", " ({hostname})"}
+            }
+        div { display: "flex", flex_direction: "row", color: "gray",
+        div {"{score}"}
+        div {padding_left: "0.5rem", "by {by}"}
+        div {padding_left: "0.5rem", "{time}"}
+        div {padding_left: "0.5rem", "{comments}"}
+    }
+            }
+        }
 }
